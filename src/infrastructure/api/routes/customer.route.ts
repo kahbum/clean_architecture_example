eeb9323803d851f36f885c9f8c5e4970d3ba3135
Tiago <t.oliveira.carmo@gmail.com>
2025-02-11 15:21:27 -0300

    feat: adding customer XML presenter

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import { json } from "sequelize";
+import CustomerPresenter from "../presenters/customer.presenter";
 
 export const customerRoute = express.Router();
 
@@ -28,7 +30,10 @@ customerRoute.get("/", async (req: Request, res: Response) => {
     const useCase = new ListCustomerUseCase(new CustomerRepository());
     try {
         const output = await useCase.execute({});
-        res.send(output);
+        res.format({
+            json: async () => res.send(output),
+            xml: async () => res.send(CustomerPresenter.listXML(output)),
+        });
     } catch (err) {
         res.status(500).send(err);
     }
